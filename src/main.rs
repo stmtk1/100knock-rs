a79d2cfebab7193515b35ef622a3ebb9d5f2d37e
斎藤基樹 <stmtk13044032@gmail.com>
2020-05-16 15:17:45 +0900

    50 done

--- src/main.rs
+++ src/main.rs
@@ -1,164 +1,50 @@
-use cabocha::parser::Parser;
-use regex::Regex;
-use itertools::Itertools;
+use std::fs::{ self, File, OpenOptions };
+use rand::seq::SliceRandom;
+use std::io::{ Write, BufWriter };
 
-fn main() {
-    //let res_txt = Parser::new(String::from("-f1")).parse_to_str(String::from("しかしその当時は考えもなかったから別段私は恐ろしいとも思わなかった。"));
-    //let res_txt = Parser::new(String::from("-f1")).parse_to_str(String::from("吾輩はここで始めて人間というものを見た。"));
-    //let res_txt = Parser::new(String::from("-f1")).parse_to_str(String::from("別段くるにも及ばんさと、主人は手紙に返事をする。"));
-    let res_txt = Parser::new(String::from("-f1")).parse_to_str(String::from("吾輩はここで始めて人間というものを見た。"));
-    let chunks: Vec<Chunk> = Chunk::new(res_txt);
-    let verb: &Chunk = (&chunks).into_iter().rev().find(|w| w.include_pos(&String::from("動詞"))).unwrap();
-    let noun: &Chunk = (&chunks).into_iter().rev().find(|w| match w.parent { None => false, Some(pid) => pid == verb.id }).unwrap();
+const P1: &str = "Reuters";
+const P2: &str = "Huffington Post";
+const P3: &str = "Businessweek";
+const P4: &str = "Contactmusic.com";
+const P5: &str = "Daily Mail";
 
-    for chunk in &chunks {
-        if let Some(pid) = chunk.parent {
-            chunk.print_path(&chunks);
-        }
-    }
-}
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let content = fs::read_to_string("./newsCorpora.csv")?;
+    let lines: Vec<Vec<String>> = content.split('\n').map(|s| s.split('\t').map(|a| String::from(a)).collect()).collect();
+    let mut news: Vec<News> = lines.into_iter().filter(|v| v.len() >= 8 && (P1 == v[3] || P2 == v[3] || P3 == v[3] || P4 == v[3] || P5 == v[3])).map(|s| News::new(s)).collect();
+    let mut train = BufWriter::new(OpenOptions::new().create(true).append(true).open("train.txt")?);
+    let mut valid = BufWriter::new(OpenOptions::new().create(true).append(true).open("valid.txt")?);
+    let mut test = BufWriter::new(OpenOptions::new().create(true).append(true).open("test.txt")?);
 
-#[derive(Debug, Clone)]
-enum Morph {
-    Word {
-        surface: String,
-        pos: String,
-        base: String,
-        pos1: String,
-    },
-    Section{
-        id: usize,
-        parent: Option<usize>,
-    },
-}
+    let mut rng = rand::thread_rng();
+    news.shuffle(&mut rng);
 
-#[derive(Debug, Clone)]
-struct Word {
-    surface: String,
-    pos: String,
-    base: String,
-    pos1: String,
-}
-
-impl Morph {
-    fn new(s :String) -> Option<Morph> {
-        let parts: Vec<String> = Regex::new(r"[\s,]").unwrap().split(&format!("{}", s)).map(|a| String::from(a)).collect();
-        if parts.len() <= 1 {
-            None
-        } else if parts[0] == String::from("*")  {
-            let parent_id: i64 = parts[2].replace("D", "").parse().unwrap();
-            let parent = if parent_id < 0 {
-                None
-            } else {
-                Some(parent_id as usize)
-            };
-            Some(Morph::Section{ 
-                id: parts[1].parse().unwrap(),
-                parent
-            })
+    for (i, n) in (&news).into_iter().enumerate() {
+        if i < news.len() / 10 * 8 {
+            train.write(format!("{}\t{}\n", n.category, n.title).as_bytes())?;
+        } else if i < news.len() / 10 * 9 {
+            valid.write(format!("{}\t{}\n", n.category, n.title).as_bytes())?;
         } else {
-            Some(Morph::Word {
-                surface: parts[0].clone(),
-                pos: parts[1].clone(),
-                pos1: parts[2].clone(),
-                base: parts[7].clone(),
-            })
+            test.write(format!("{}\t{}\n", n.category, n.title).as_bytes())?;
         }
     }
 
-    fn id(&self) -> Option<usize> {
-        match self {
-            Morph::Section{ id, .. } => Some(*id),
-            Morph::Word{..} => None,
-        }
-    }
-
-    fn parent(&self) -> Option<Option<usize>> {
-        match self {
-            Morph::Section{ parent, .. } => Some(*parent),
-            Morph::Word{..} => None,
-        }
-    }
+    train.flush()?;
+    test.flush()?;
+    valid.flush()?;
+    Ok(())
 }
 
-impl Word {
-    fn new(morph: &Morph) -> Option<Word> {
-        match morph {
-            Morph::Section{..} => None,
-            Morph::Word { surface, pos, pos1, base } => {
-                Some(Word{
-                    surface: surface.clone(),
-                    pos: pos.clone(),
-                    pos1: pos1.clone(),
-                    base: base.clone(),
-                })
-            }
-        }
-    }
+struct News {
+    category: String,
+    title: String,
 }
 
-#[derive(Debug, Clone)]
-struct Chunk {
-    id: usize,
-    parent: Option<usize>,
-    words: Vec<Word>,
-}
-
-impl Chunk {
-    fn new(s: String) -> Vec<Chunk> {
-        let mut ret: Vec<Chunk> = Vec::new();
-        let words: Vec<Morph> = s.split("\n").into_iter().map(|s| Morph::new(String::from(s))).filter(|w| w.is_some()).map(|w| w.unwrap()).collect();
-        let first_id = words[0].id().unwrap();
-        let first_parent = words[0].parent().unwrap();
-        let mut chunk = Chunk { id: first_id, words: Vec::new(), parent: first_parent };
-        for w in words.into_iter().skip(1) {
-            match w {
-                Morph::Section{ id, parent } => {
-                    ret.push(chunk);
-                    chunk = Chunk {
-                        id,
-                        parent,
-                        words: Vec::new(),
-                    };
-                },
-                Morph::Word{..} => {
-                    let word = Word::new(&w).unwrap();
-                    if word.pos != "記号" {
-                        chunk.words.push(word);
-                    }
-                }
-            }
-        }
-        ret.push(chunk);
-        ret
-    }
-
-    fn print_words(&self) {
-        for word in &self.words {
-            let Word { surface, .. } = word;
-            print!("{}", surface);
+impl News {
+    fn new(s: Vec<String>) -> News {
+        News {
+            category: s[4].clone(),
+            title: s[1].clone(),
         }
     }
-
-    fn join_words(&self) -> String {
-        (&self.words).into_iter().map(|w| &w.surface as &str).intersperse("").collect()
-    }
-
-     fn include_pos(&self, pos: &String) -> bool {
-         (&self.words).into_iter().find(|w| &w.pos == pos ).is_some()
-     }
-
-     fn find_last_pos(&self, pos: String) -> Option<Word> {
-         (&self.words).into_iter().rev().find(|w| w.pos == pos).and_then(|w| Some(w.clone()))
-     }
-
-     fn print_path(&self, chunks: &Vec<Chunk>) {
-         match self.parent {
-             None => println!("{}", self.join_words()),
-             Some(pid) => {
-                 print!("{} -> ", self.join_words());
-                 chunks[pid].print_path(&chunks);
-             }
-         }
-     }
 }
